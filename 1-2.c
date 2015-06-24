ccc0b21351a19e01aa10a2961b0380a848b83d83
Michael James <michaelspacejames@gmail.com>
2015-06-24 13:09:35 +0530

    Some legal escape sequences

---
+++ 1-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	printf("Alarm: \a\n");
+	printf("Formfeed: \f\n");
+	printf("Carriage Return: \r\n");
+	printf("Vertical tab: \v\n");
+	printf("Single quotation mark: \'\n");
+	printf("Question mark: \?\n");
+
+}
