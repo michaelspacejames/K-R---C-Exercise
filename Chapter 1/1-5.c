b778d07a91150f5c978d8c5e0804502353e8f56b
Michael James <michaelspacejames@gmail.com>
2015-06-26 09:08:09 +0530

    Made a table with asteriks

--- Chapter 1/1-5.c
+++ Chapter 1/1-5.c
@@ -6,13 +6,15 @@ int main(void)
 
 	float fahr;
 
-	printf("Fahrenheit\tCelsius\n");
+	printf("*****************************\n*  Fahrenheit  *  Celsius   *\n*****************************\n");
 
 	for(fahr = 300; fahr >= 0; fahr -= 20)
 	{	
 
-		printf("%10.0f\t%7.1f\n", fahr, 5.0/9.0 * (fahr - 32));
+		printf("*   %9.0f  *  %7.1f   *\n", fahr, 5.0/9.0 * (fahr - 32));
 
 	}
+
+	printf("*****************************\n");
 
 }
