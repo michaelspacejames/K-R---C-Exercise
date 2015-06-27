818874dfa0150966bea945dc815bf17654013251
Michael James <michaelspacejames@gmail.com>
2015-06-27 08:32:21 +0530

    Done

---
+++ Chapter 1/1-10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int c;
+	while((c = getchar()) != EOF)
+	{
+		if(c == '\t')
+		{
+			putchar('\\');
+			putchar('t');
+		}
+
+		else if(c == '\b') 
+		{
+			putchar('\\');
+			putchar('b');
+		}
+
+		else if(c == '\\')
+		{
+			putchar('\\');
+			putchar('\\');
+
+		}
+		
+		else
+			putchar(c);
+
+	}
+
+}
